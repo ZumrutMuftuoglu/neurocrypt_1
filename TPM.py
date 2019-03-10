d499ae097ac58a717cb3b5bf3b0c1078cf5dca1e
Zumrut Muftuoglu <35799127+ZumrutMuftuoglu@users.noreply.github.com>
2019-03-10 04:59:18 -0700

    Update TPM.py

--- TPM.py
+++ TPM.py
@@ -1,5 +1,5 @@
-#Burada TPM'i oluşturuyoruz
-class Machine:
+#TPM'i oluşturur.
+class TPM:
     '''Burada TPM'i oluşturuyoruz. TPM binary çıkış üretir(tau).
     Parametreler :
     
@@ -9,31 +9,26 @@ class Machine:
     W - k*n boyutlu ağırlık matrisi
     '''
 #ilklendirme (ağ topolojisi burada belirlenir)
-    def __init__(self, k=3, n=4, d=6):
-        
+    def __init__(self, k=3, n=4, d=6):        
         self.k = k
         self.n = n
         self.d = d
         self.W = np.random.randint(-d, d + 1, [k, n])
-
     def get_output(self, X):
+        
         '''
         X - Rastgele giriş vektörü
         Buradaki işlem {-1,1} değerlerini döndürür
         '''
-
         k = self.k
         n = self.n
         W = self.W
         X = X.reshape([k, n])
-
         sigma = np.sign(np.sum(X * W, axis=1)) # sigmayı hesaplar
         tau = np.prod(sigma) # çıkışa götürür
-
         self.X = X
         self.sigma = sigma
         self.tau = tau
-
         return tau
 
     def __call__(self, X):
@@ -43,15 +38,14 @@ class Machine:
         
         '''
         Burada kullandığımız öğrenme kuralına göre ağırlıkları güncelliyoruz.
-        tau2 - diğer makinenin çıkışına götürür
-        
+        tau2 - diğer makinenin çıkışına götürür        
         '''
         X = self.X
         tau1 = self.tau
         sigma = self.sigma
         W = self.W
         d = self.d
-
+        
         if (tau1 == tau2):
             if rule == 'hebbian':
                 hebbian(W, X, sigma, tau1, tau2, d)            
